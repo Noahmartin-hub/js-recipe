f9932902bf3eef1d4ecb8ab133af117e40f0a389
Noahmartin-hub <noanoa_n.y@keio.jp>
2021-01-15 19:58:48 +0900

    コミット

---
+++ 1_workspace/2021 1.15/main.js
@@ -0,0 +1,27 @@
+const button1=document.getElementById("button1")
+const button2=document.getElementById("button2")
+const button3=document.getElementById("button3")
+const feedback=document.getElementById("feedback")
+
+button1.onclick = function() {
+  feedback.textContent = "こんなに下手じゃねえわｗ"
+}
+button2.onclick = function() {
+  feedback.textContent = "もとうまいんだよなあ"
+}
+button3.onclick = function() {
+  feedback.textContent = "あたりでーすわーい"
+}
+
+const noah = {
+  name:"のあ",
+  age:21,
+  birthDay:"1999/09/02",
+  hobbies:["basketball","workout","golf"],
+
+}
+
+console.log(Noah.name)
+console.log(Noah.age)
+console.log(Noah.birthDay)
+

ec8a525af54ae3dcbe676336662026dfe6fd3dd6
Noahmartin-hub <noanoa_n.y@keio.jp>
2021-03-07 13:43:59 +0900

    コミット

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,27 +1,12 @@
-const genkiFunction = function(number){
+const genkiFunction = function(number) {
   // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
-  for (let n=1; n<=number; n++) {
+  for (let n = 1; n <= number; n++) {
     if (n % 3 === 0) {
-      console.log(`${n}!!!!!!!`)
+      console.log("Fizz")
+    } else if (n % 5 === 0) {
+      console.log("Buzz")
     } else {
       console.log(n)
     }
   }
 }
-
-const kisuu = function (n){
-  return 2 * n + 1
-}
-
-kisuu (5)
-
-const plus = function (x, y) {
-  return x + y
-}
-plus (11, 31) 
-
-const multiply = function (x, y) {
-  return x * y 
-}
-
-genkiFunction(100)
--- 1_workspace/2/犬/main.js
+++ 1_workspace/2/犬/main.js
@@ -1,10 +1,12 @@
-const imageElement = document.getElementById("dog-image")
+const imageElement = document.getElementById("cat-image")
 
 // 指定したサーバーにデータを取りに行く
-fetch("https://dog.ceo/api/breeds/image/random")
+fetch("https://api.thecatapi.com/v1/images/search")
   .then((res) => {
+    // fetch がおわったら１つめの then の中の関数を実行し、
     return res.json() // 結果を json として読み込む
   })
   .then((data) => {
+    // １つめが終わったら２つめの then の中の関数を実行します。
     imageElement.src = data.message // 画像を表示する
   })
